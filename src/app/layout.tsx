1931cf62f48cef591ada893e97891b29db6e7ff0
Gyanesh <115879950+Gyanesh2003@users.noreply.github.com>
2025-06-16 22:19:40 +0530

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -29,7 +29,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        <meta name="google-site-verification" content="google96cd18bcda13e46b" />
+        <meta name="google-site-verification" content="google96cd18bcda13e46b.html" />
       </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
